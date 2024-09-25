27d246359750636b09c81ea771188852d7c5236d
Nandinee Patel <nandineepatel2003@gmail.com>
2024-09-25 10:17:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// Read the port from the environment or fallback to 5173
+const port = process.env.PORT || 5173;
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,6 +13,8 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',  // Bind to all network interfaces
+    port: parseInt(port),  // Use the port defined in the environment
     proxy: {
       '/api': {
         target: 'https://maps.googleapis.com',
